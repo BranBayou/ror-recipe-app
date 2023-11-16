2b2d8ac2d82a90315a505e9401de8991ce8d45c8
rbd3 <rabedesanaandry01@gmail.com>
2023-11-16 20:07:28 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
+
+  resources :users, only: [:index, :show, :new, :create, :destroy]
+  resources :foods
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+
+  resources :recipes do
+    resources :recipe_foods, only: [:index, :show, :new, :create, :destroy]
+  end
 end
